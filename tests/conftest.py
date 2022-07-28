3645d5914e99c755b2af672a1e5a8e6a3febadef
Sebastian Kowalak <skowalak@phoenixcontact-sb.io>
2022-07-29 00:51:20 +0200

    feat: 🧪 restructure tests and adapt conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,28 +6,10 @@ import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-DB_URL = "sqlite://:memory:"
-TESTUSER_UID = "2d12fe46-380d-40ba-815a-edc0a606281c"
-TESTUSER__FN = "Carlos Matos"
-TESTUSER_EML = "d70a6635-edac-42ea-81e6-9db15252802a@invalid.tld"
-
-# hook called after creation of session object, before collection
-def pytest_sessionstart(session: pytest.Session):
-    # inject sqlite memdb here
-    os.environ["PSQL_URL"] = DB_URL
-
-
-async def init_db(db_url: str, create_db: bool = False, schemas: bool = False) -> None:
-    # create database here
-    if create_db:
-        print(f"PYTEST: Database created. {db_url=}")
-    if schemas:
-        await Tortoise.generate_schemas()
-        print(f"PYTEST: Schemas generated.")
-
-
-async def init(db_url: str = DB_URL) -> None:
-    pass  # empty, because database is initiated by service.
+os.environ.update({
+    "HOSTNAME": "asdfghjkl",
+    "NUM_REPLICAS": "1",   
+})
 
 
 @pytest.fixture(scope="session")
@@ -40,38 +22,17 @@ def event_loop():
 
 @pytest.fixture(scope="session", autouse=True)
 async def initialize_tests():
-    await init()
     yield
-    # tear down database here
-
-
-@pytest.fixture(scope="session")
-async def current_user():
-    from app.api.auth import User
-
-    return User(
-        id=TESTUSER_UID,
-        full_name=TESTUSER__FN,
-        email=TESTUSER_EML,
-    )
 
 
 @pytest.fixture(scope="function")
-def app(current_user):
+def app():
     """
     Generate a new FastAPI app instance for every test case.
     """
     from app import main
-    from app.api.auth import User, get_auth, get_user
 
     app = main.app
-    # include _old_ auth dep for compatibility
-    app.dependency_overrides[get_auth] = lambda: {"sub": current_user.id}
-    auth_deps = [
-        get_user,
-    ]
-    for dep in auth_deps:
-        app.dependency_overrides[dep] = lambda: current_user
 
     return app
 
@@ -87,3 +48,4 @@ def settings():
     from app.config import get_settings
 
     return get_settings()
+
--- tests/integration/test_pytest.py
+++ tests/integration/test_pytest.py
@@ -3,9 +3,9 @@ from fastapi.testclient import TestClient
 
 
 @pytest.mark.asyncio
-async def test_get_root(client: TestClient):
-    response = client.get("/")
-    assert response.status_code == 404
+async def test_get_health(client: TestClient):
+    response = client.get("/api/v1/health")
+    assert response.status_code == 200
 
 
 @pytest.mark.asyncio
