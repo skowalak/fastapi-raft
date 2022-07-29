444f76b591aed3aae34acd2a7734a1b99c8c8471
Sebastian Kowalak <skowalak@phoenixcontact-sb.io>
2022-07-29 11:07:32 +0200

    feat(tests): 🧪 skip integr tests add unit tests

--- tests/integration/test_pytest.py
+++ tests/integration/test_pytest.py
@@ -2,13 +2,19 @@ import pytest
 from fastapi.testclient import TestClient
 
 
-@pytest.mark.asyncio
-async def test_get_health(client: TestClient):
-    response = client.get("/api/v1/health")
-    assert response.status_code == 200
+class TestBasicRoutes:
+    pytestmark = pytest.mark.skip(
+        reason="currently not working due to discovery mechanism"
+    )
 
+    @pytest.mark.asyncio
+    @pytest.mark.skip(reason="")
+    async def test_get_health(client: TestClient):
+        response = client.get("/api/v1/health")
+        assert response.status_code == 200
 
-@pytest.mark.asyncio
-async def test_get_invalid_path(client: TestClient):
-    response = client.get("/wkkekfnoi")
-    assert response.status_code == 404
+    @pytest.mark.asyncio
+    @pytest.mark.skip(reason="")
+    async def test_get_invalid_path(client: TestClient):
+        response = client.get("/wkkekfnoi")
+        assert response.status_code == 404
--- tests/integration/test_swaggerdoc.py
+++ tests/integration/test_swaggerdoc.py
@@ -1,12 +1,21 @@
+import pytest
+
 from fastapi.testclient import TestClient
 from app.config import Settings
 
 
-def test_docs_html(client: TestClient, settings: Settings):
-    response = client.get(settings.FASTAPI_DOCS)
-    assert response.status_code == 200
+class TestSwaggerDocumentation:
+    pytestmark = pytest.mark.skip(reason="does not work with discovery by DNS")
 
+    @pytest.mark.asyncio
+    @pytest.mark.skip(reason="")
+    async def test_docs_html(client: TestClient, settings: Settings):
+        response = client.get(settings.FASTAPI_DOCS)
+        assert response.status_code == 200
 
-def test_get_health(client: TestClient, settings: Settings):
-    response = client.get(settings.FASTAPI_SCHEM)
-    assert response.status_code == 200
+    @pytest.mark.asyncio
+    @pytest.mark.skip(reason="")
+    async def test_openapi(client: TestClient, settings: Settings):
+        response = client.get(settings.FASTAPI_SCHEM)
+        assert response.status_code == 200
+        assert response.content_type == "application/json"
